81f5784600dc415b7e3cebbb8bb292ce50da4b68
Aaditya Sharma <aaditya.sharma@conneqtiongroup.com>
2024-01-07 13:57:35 +0530

    addFile

--- 3 Todo App/src/App.jsx
+++ 3 Todo App/src/App.jsx
@@ -3,20 +3,42 @@ import AddTodo from './components/AddTodo';
 import TodoItems from './components/TodoItems';
 import WelcomeMsg from './components/WelcomeMsg';
 import "./App.css";
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { MdFileDownload } from "react-icons/md";
 import { TodoItemsContext } from './store/todo-items-store';
 
 
+const todoItemReducer = (currTodoItems,action)=>{
+  let newTodoItems=currTodoItems;
+  if(action.type==='NEW_ITEM'){
+      newTodoItems=[...currTodoItems,{name:action.payload.itemName,
+      dueDate:action.payload.itemDueDate}];
+  }
+  else if(action.type==='DELETE_ITEM'){
+
+  }
+  return newTodoItems;
+}
+
 function App() {
  
-  const [todoItemList,setTodoItemList]=useState([]);
+  //const [todoItemList,setTodoItemList]=useState([]);
+  const [todoItemList,dispatchTodoItem] = useReducer(todoItemReducer,[]);
 
   const addToDoItemsFun=(itemName,itemDueDate)=>{
+
+    const newItemAction = {
+      type:"NEW_ITEM",
+      payload:{
+        itemName,
+        itemDueDate
+      }
+    };
+    dispatchTodoItem(newItemAction);
   
-    let newTodoItems=[...todoItemList,{name:itemName,
-    dueDate:itemDueDate}];
-    setTodoItemList(newTodoItems);  
+    // let newTodoItems=[...todoItemList,{name:itemName,
+    // dueDate:itemDueDate}];
+    // setTodoItemList(newTodoItems);  
 
   };
 
